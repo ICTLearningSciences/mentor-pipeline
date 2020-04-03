7325d4b376e6ff1de1e395b6235d31fbb9c5b79f
larry kirschner <larrykirschner@gmail.com>
2020-04-03 16:02:23 -0700

    noise reduction must force audio to mono or it will crash

--- mentor_pipeline/noise.py
+++ mentor_pipeline/noise.py
@@ -7,23 +7,29 @@ import soundfile as sf
 from typing import Iterable, Union
 
 
-def _reduce_noise(noise_sample: np.ndarray, f: Union[str, os.PathLike]):
+def _reduce_noise(
+    noise_sample: np.ndarray, f: Union[str, os.PathLike], decrease_by=0.5
+):
     f = os.path.abspath(f)
     fpath, fext = os.path.splitext(f)
     audio_output_file = f"{fpath}.wav"
     audio_input_file = f"{fpath}-prenoisefix.wav"
     if fext != ".wav":
-        ffmpy.FFmpeg(inputs={f: None}, outputs={audio_input_file: None}).run()
+        ffmpy.FFmpeg(inputs={f: None}, outputs={audio_input_file: "-y -ac 1"}).run()
     data, rate = sf.read(audio_input_file)
+    # noise_sample = data[0:1000]
     reduced_noise = nr.reduce_noise(
-        audio_clip=data, noise_clip=noise_sample, verbose=True
+        audio_clip=data,
+        noise_clip=noise_sample,
+        prop_decrease=decrease_by,
+        verbose=False,
     )
     sf.write(audio_output_file, reduced_noise, rate)
     if fext == ".mp4":
         tmp_file = f"{fpath}.tmp{fext}"
         ffmpy.FFmpeg(
             inputs={f: None, audio_output_file: None},
-            outputs={tmp_file: "-c:v copy -map 0:v:0 -map 1:a:0"},
+            outputs={tmp_file: "-y -c:v copy -map 0:v:0 -map 1:a:0"},
         ).run()
         os.rename(f, f"{fpath}-prenoisefix{fext}")
         os.rename(tmp_file, f)
@@ -32,7 +38,9 @@ def _reduce_noise(noise_sample: np.ndarray, f: Union[str, os.PathLike]):
 
 
 def reduce_noise(
-    noise_sample: str, files_to_fix: Union[str, Iterable[os.PathLike], os.PathLike]
+    noise_sample: str,
+    files_to_fix: Union[str, Iterable[os.PathLike], os.PathLike],
+    decrease_by=0.75,
 ):
     noise, _ = sf.read(noise_sample)
     for f in (
@@ -40,4 +48,4 @@ def reduce_noise(
         if isinstance(files_to_fix, str)
         else files_to_fix
     ):
-        _reduce_noise(noise, f)
+        _reduce_noise(noise, f, decrease_by=decrease_by)
