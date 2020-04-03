e5df614e5c503cba3c29d1f49471908229fb7ad6
larry kirschner <larrykirschner@gmail.com>
2020-04-03 15:30:50 -0700

    fixes: noise-reduce should not rename original file unless success

--- mentor_pipeline/noise.py
+++ mentor_pipeline/noise.py
@@ -10,22 +10,25 @@ from typing import Iterable, Union
 def _reduce_noise(noise_sample: np.ndarray, f: Union[str, os.PathLike]):
     f = os.path.abspath(f)
     fpath, fext = os.path.splitext(f)
-    save_file = f"{fpath}-prenoisefix{fext}"
-    os.rename(f, save_file)
     audio_output_file = f"{fpath}.wav"
     audio_input_file = f"{fpath}-prenoisefix.wav"
     if fext != ".wav":
-        ffmpy.FFmpeg(inputs={save_file: None}, outputs={audio_input_file: None}).run()
+        ffmpy.FFmpeg(inputs={f: None}, outputs={audio_input_file: None}).run()
     data, rate = sf.read(audio_input_file)
     reduced_noise = nr.reduce_noise(
-        audio_clip=data, noise_clip=noise_sample, verbose=False
+        audio_clip=data, noise_clip=noise_sample, verbose=True
     )
     sf.write(audio_output_file, reduced_noise, rate)
     if fext == ".mp4":
+        tmp_file = f"{fpath}.tmp{fext}"
         ffmpy.FFmpeg(
-            inputs={save_file: None, audio_output_file: None},
-            outputs={f: "-c:v copy -map 0:v:0 -map 1:a:0"},
+            inputs={f: None, audio_output_file: None},
+            outputs={tmp_file: "-c:v copy -map 0:v:0 -map 1:a:0"},
         ).run()
+        os.rename(f, f"{fpath}-prenoisefix{fext}")
+        os.rename(tmp_file, f)
+        os.remove(audio_input_file)
+        os.remove(audio_output_file)
 
 
 def reduce_noise(
